1f74bbe83ff2e026dba1a8f41ed020f311e16748
Vanessa Nascimento <101233011+Nayayaa@users.noreply.github.com>
2023-03-20 09:34:48 -0300

    Aluraflix

---
+++ aluraflix/README.md
@@ -0,0 +1,4 @@
+# AluraFlix 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nayayaa/pen/LYeZQWV](https://codepen.io/nayayaa/pen/LYeZQWV).
+
---
+++ aluraflix/src/script.js
@@ -0,0 +1,14 @@
+var listaAnime = [
+  "https://i0.wp.com/www.otakupt.com/wp-content/uploads/2018/07/Nova-imagem-promocional-de-Yagate-Kimi-ni-Naru-1.jpg?resize=640%2C904&ssl=1",
+  "https://i.pinimg.com/originals/ac/f7/fe/acf7fe008403cede13251b661ffd72cc.jpg",
+  "https://images.justwatch.com/poster/253938038/s718",
+  "http://vortexcultural.com.br/images/2016/07/neon-genesis-evangelion.jpg",
+  "https://m.media-amazon.com/images/M/MV5BOTg5ZjI5ZTAtOWE2OS00MjY4LWI4ZDQtMzEzMDY4MTgzYWJlXkEyXkFqcGdeQXVyNjc3OTE4Nzk@._V1_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BNWZlYWVjMGMtNjFjNi00ZTZiLWI2ZGEtMThjYjRmY2M2N2RiXkEyXkFqcGdeQXVyMzgzOTI1NTA@._V1_.jpg",
+  "https://es.web.img2.acsta.net/pictures/20/11/03/12/11/0600565.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMGNmMWI4MGUtYmU0ZS00ZDkxLTgzMTAtZDU4YmQ1MDM3Y2IyXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg"
+];
+
+for (var i = 0; i < listaAnime.length; i++) {
+  document.write("<img src=" + listaAnime[i] + ">");
+}
