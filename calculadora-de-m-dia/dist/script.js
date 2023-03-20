443ba339268c1e27d2969237401f02d0ec8e1fb6
Vanessa Nascimento <101233011+Nayayaa@users.noreply.github.com>
2023-03-20 09:26:32 -0300

    Exercício de Calculadora.

---
+++ calculadora-de-m-dia/dist/script.js
@@ -0,0 +1,20 @@
+document.getElementById("bnt").onclick = function (e) {
+  var valorNotaDoPrimeiroBimestre = parseFloat(document.getElementById ("nota1").value);
+  var valorNotaDoSegundoBimestre = parseFloat(document.getElementById ("nota2").value);
+  var valorNotaDoTerceiroBimestre = parseFloat(document.getElementById ("nota3").value);
+  var valorNotaDoQuartoBimestre = parseFloat(document.getElementById ("nota4").value);
+  
+  var notaFinal = ((valorNotaDoPrimeiroBimestre + valorNotaDoSegundoBimestre + valorNotaDoTerceiroBimestre + valorNotaDoQuartoBimestre) / 4).toFixed(1);
+  
+  let resultado;
+  
+  if (notaFinal >=7){
+      resultado = "Aprovado";
+    }
+ 
+  else{
+    resultado = "Reprovado"
+    }
+  
+  document.getElementById("resultado").innerHTML = `Nota final deu <em>${notaFinal} </em> - <em>${resultado}</em>`
+ }
